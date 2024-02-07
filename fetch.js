3970a48af238e3432f2b38d94851d3f06961befe
Paul <paul.abgrall@supdevinci-edu.fr>
2024-02-07 09:18:34 +0100

    ajout de la barre de recherche fonctionnelle

--- fetch.js
+++ fetch.js
@@ -1,29 +1,24 @@
-
-async function request_trivia(nb) {
-    let response = await fetch('http://numbersapi.com/'+nb+'?json');
-    let json = await response.json();
-    console.log(json);
+window.onload = function() { //Au chargement de la page, création d'une fonction anonyme:
+  let bt = document.getElementById("Bouton"); //on récupère le bouton HTML pour l'attribuer à bt
+  bt.addEventListener("click", () => { "" //Si le boutton est clické, on execute la fonction request
+    request();
+  });
 }
 
-async function request_math(nb) {
-  let response = await fetch('http://numbersapi.com/'+nb+'/math?json');
+
+async function request() { //Fonction de requête vers l'API
+  let nombre = document.getElementById("nombre").value; //On créer nombre qui récupère le texte dans la bare de recherche
+  let selection = document.querySelectorAll('input[name="selection"]'); //On créer selection qui correspond à toutes les cases de sélection (Trivia,Math...)
+  let selected = "" //On reset selected
+
+  for (let i = 0; i < selection.length; i++) {  //Boucle pour parcourir toutes les valeurs de selection
+    if (selection[i].checked) {  //Si la valeur de selection == true c'est qu'elle est cochée
+      selected = selection[i].value //Donc on nomme selected par la case cochée
+        break
+    }
+  }
+
+  let response = await fetch('http://numbersapi.com/'+nombre+'/'+selected+'?json'); //Requête vers l'API avec les différents paramètres 
   let json = await response.json();
   console.log(json);
 }
-
-async function request_date(nb) {
-  let response = await fetch('http://numbersapi.com/'+nb+'/date?json');
-  let json = await response.json();
-  console.log(json);
-}
-
-async function request_year(nb) {
-  let response = await fetch('http://numbersapi.com/'+nb+'/year?json');
-  let json = await response.json();
-  console.log(json);
-}
-
-//request_trivia(78);
-//request_math(44);
-request_date(22);
-// request_year(4);
